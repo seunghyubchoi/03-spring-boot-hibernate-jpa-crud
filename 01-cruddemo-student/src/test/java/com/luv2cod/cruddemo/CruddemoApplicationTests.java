b52da5c493831d4149ece9720323e67d98d5c208
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-11-06 08:17:07 +0900

    mysql set

---
+++ 01-cruddemo-student/src/test/java/com/luv2cod/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2cod.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

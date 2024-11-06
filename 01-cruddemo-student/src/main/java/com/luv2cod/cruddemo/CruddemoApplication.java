f38928df9e0560ba19bf902d4fa35e9f32c3e8bc
Seunghyub Choi <seunghyub.choi@gmail.com>
2024-11-06 23:45:39 +0900

    entitymanager

--- 01-cruddemo-student/src/main/java/com/luv2cod/cruddemo/CruddemoApplication.java
+++ 01-cruddemo-student/src/main/java/com/luv2cod/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2cod.cruddemo;
 
+import com.luv2cod.cruddemo.dao.StudentDAO;
+import com.luv2cod.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,23 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		// create the student object
+		System.out.println("Create new student object...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+
+		// save the student object
+		System.out.println("Saving the student...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id : " + tempStudent.getId());
 	}
 
 }
---
+++ 01-cruddemo-student/src/main/java/com/luv2cod/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,9 @@
+package com.luv2cod.cruddemo.dao;
+
+import com.luv2cod.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student student);
+
+}
